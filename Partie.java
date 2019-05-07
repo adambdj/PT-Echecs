71b2899409a05383be59f9e1556257972cac31d6
Adam Bouarroudj <48649017+adambdj@users.noreply.github.com>
2019-05-07 23:54:22 +0200

    Harmonisation
    Harmonisation du code pour plus de clarté.

--- Partie.java
+++ Partie.java
@@ -1,28 +1,32 @@
 //Ce qu'on peux faire pendant une partie
 //une partie est composé d'un echiquier
-public class Partie {
+public class Partie
+{
 	private Echiquier terrain; //Une Partie est composé d'un Echequier
 	private Joueur joueur1;
 	private Joueur joueur2;
 
 	
-	public Partie() {
+	public Partie()
+	{
 		this.terrain=new Echequier();
 	}
 	
 //--------------Soumeya
 	/*Le jeu annonce un MAT lorsque le Roi est attaqué et qu’il n’y a aucune possibilité de le sauver.*/
-	public boolean verifMAT(){
-
+	public boolean verifMAT()
+	{
 	}
 
 	/*	Le jeu annonce un PAT lorsque le joueur ne peut déplacer aucune pièces 
 	 *	et que le roi n’est pas en attaque et ne peut se déplacer sans se mettre en échec .*/
 
-	public boolean verifPAT(){
-
+	public boolean verifPAT()
+	{
 	}
-	public boolean verifDeplacement(Piece p) {
+	
+	public boolean verifDeplacement(Piece p)
+	{
 		boolean reponse = false;
 		if(p instanceof Tour) //Si la piece est une tour
 		{
@@ -72,37 +76,38 @@ public class Partie {
 //-------------------
 
 
-	public void lancerPartie(){
-
+	public void lancerPartie()
+	{
 	}
 
-	public void sauvegarderPartie(){
-
+	public void sauvegarderPartie()
+	{
 	}
 
-	public void restaurerPartie(){
-
+	public void restaurerPartie()
+	{
 	}
 
-	public void effacerTerrain(){
-
+	public void effacerTerrain()
+	{
 	}
 
-	public void historique(){
-
+	public void historique()
+	{
 	}
 
-	public void tourDeJeu(){
-		
+	public void tourDeJeu()
+	{
 	}
 
-	public Echequier getTerrain() {
+	public Echequier getTerrain()
+	{
 		return terrain;
 	}
 
-	public void setTerrain(Echequier nouvTerrain) {
+	public void setTerrain(Echequier nouvTerrain)
+	{
 		this.terrain = nouvTerrain;
 	}
-	
 	
 }
