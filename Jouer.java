1793a9bb646d32563a48b3473a2ec4b151b37fbc
Adam Bouarroudj <48649017+adambdj@users.noreply.github.com>
2019-05-07 23:51:49 +0200

    Update Jouer.java

--- Jouer.java
+++ Jouer.java
@@ -5,31 +5,28 @@ public class Jouer
         System.out.println("AAAAAAAAAAAAAAAAA");
     }
 
-
-
-
     /*
-   		Comment se déroule une partie ? 
+   	Comment se déroule une partie ? 
 	-> On lance la partie (fonction : lancer Partie)
 	-> JOUEUR 1 : 
 		->"ql pièce voulez-vous déplacer ? "
-		apelle fonction : saisie return coordonnée
-		if Existe coordonnée == poisitonPiece (classe piece) 
+		appelle fonction : saisie return coordonnée
+		if Existe coordonnée == positionPiece (classe piece) 
 
 		-> "où voulez-vous vous déplacer la piece de coordonnéePosition(i,j) ?"
-		apelle fonction : saisie return coordonnée
-		apelle fonction : verifDéplacement (coordonnée) return 
-			True : apelle fonction déplacerPiece(nomPiece)
+		appelle fonction : saisie return coordonnée
+		appelle fonction : verifDéplacement (coordonnée) return 
+			True : appelle fonction déplacerPiece(nomPiece)
 					"déplacement effectué" 
-				apelle fonction verifMAT/PAT return True = fin Partie ou null
+				appelle fonction verifMAT/PAT return True = fin Partie ou null
 				false : fonction d'affichage JOUEUR 2
 	
 			false : déplacement impossible !
 				return en haut de la boucle
 
 		->voulez-vous quitter la partie ?
-		apelle fonction : saisie return non - on continue
-			oui - apelle fonction : sauvegarderPartie return 
+		appelle fonction : saisie return non - on continue
+			oui - appelle fonction : sauvegarderPartie return 
 			sauvegarder partie apl verifMAT/PAT if True alors return False ON NE PEUX PAS SAUVEGARDER
 			Sinon on sauvegarde car partie non null ni FINI 
    */
