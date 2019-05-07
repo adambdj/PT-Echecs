914555f6080d02f235388956b56b978e8674b302
salimdrch <49313964+salimdrch@users.noreply.github.com>
2019-05-07 17:36:57 +0200

    Update Echiquier.java

--- Echiquier.java
+++ Echiquier.java
@@ -1,32 +1,43 @@
 public class Echiquier
 {
   private Case[][] grille;
-  private Piece[] tabPiece = {"Tour","Fou","Cavalier","Reine","Roi","Cavalier","Fou","Tour","Pion"};
   
   public Echiquier()
   {
-  	for(int i = 0; i < 8, i++)
+    this.intialiser();
+  }
+  /* Initialise le tableau */
+  public void intialiser()
+  {
+    for(int i = 0; i < 8, i++)
     {
     	for(int j = 0; j < 8; j++)
           this.grille[i][j] = new Case(i,j);
     }
+  }
+  /* getCase() */
+  public Case getCase(int x, int y)
+  {
+    return this.grille[i][j];   // retourne la case par rapport a l'indice x, y
+  }
+  /* setCase() */
+  public void setCase(int x, int y, Piece p)
+  {
   
   }
-
-//------Soumeya
-  //Permet de mettre en paramètre les coordonné d'une case pour savoir directement son etat
-  public boolean etatCase(int x, int y){
-    if (this.grille[x][y].getCaseLibre == true)
-      return true;
-    else 
-      return false;
-  }
-//-----------
-
+  /* GETTER */
   public Case[][] getGrille(){
     return this.grille;
   }
-
+//------Soumeya
+  //Permet de mettre en paramètre les coordonné d'une case pour savoir directement son etat
+  public boolean etatCase(int x, int y){
+  //  if (this.grille[x][y].getCaseLibre == true)
+  //    return true;
+  //  else 
+  //    return false;
+  }
+//-----------
 
 
 }
