91facbf41f13908367ef762391b7bc2f97f8e747
Adam Bouarroudj <48649017+adambdj@users.noreply.github.com>
2019-05-07 17:43:57 +0200

    Update Piece.java
    Mise à jour de Piece.java à partir du nouveau diagramme de classe (fait le Mardi 7 mai à la bibliothèque)

--- Piece.java
+++ Piece.java
@@ -1,75 +1,69 @@
 public abstract class Piece
 {
-  private String couleur;
-  private boolean etatPiece;
-  private int x;	// position x 
-  private int y;	// position y 
-  
-  // Contructeur vide
-  public Piece()
-  {
-  	this.couleur = "";
-    this.etatPiece = true; // true(piece en jeu) false(piece detruite)
-  }
+	private int couleur;	// 0 pour blanc, 1 pour noir
+	private int x;	// position x 
+  	private int y;	// position y 
  
-  // Constructeur avec parametre
-  public Piece(String c,int x, int y)
-  {
-  	this.couleur = c;
-    this.etatPiece = true; // true(piece en jeu) false(piece detruite)
-	  this.x = x;
-    this.y = y;
-  }
+  	// Constructeur avec paramètres
+  	
+	public Piece(int x, int y, int couleur)
+  	{
+  	}
   
-  /* GETTER */
-  public int getPosX()
-  {
-  	return this.x;
-  }
+  	/* GETTERS */
+  	public int getPosX()
+  	{
+	  	return this.x;
+  	}
   
-  public int getPosY()
-  {
-  	return this.y;
-  }
+ 	public int getPosY()
+  	{
+  		return this.y;
+  	}
   
-  public boolean getEtatPiece()
-  {
-  	return this.etatPiece;
-  }
+  	public boolean getEtatPiece()
+  	{
+  		return this.etatPiece;
+  	}
   
-  public String getCouleur()
-  {
-  	return this.couleur;
-  }
+  	public String getCouleur()
+  	{
+  		return this.couleur;
+  	}
+  	
+	/*___________________FIN DES GETTERS	*/
+	
+  	/* SETTERS */
+  	public void setPosX(int x)
+  	{
+  		this.x = x;
+  	}
   
-  /* SETTER */
-  public void setPosX(int x)
-  {
-  	this.x = x;
-  }
+  	public void setPosY(int y)
+  	{
+  		this.y = y;
+  	}
   
-  public void setPosY(int y)
-  {
-  	this.y = y;
-  }
+  	public void setEtatPiece(boolean etat)
+  	{
+  		this.etatPiece = etat;
+    		if(etat == false)
+    		{
+      			this.x = 8;
+      			this.y = 9;
+    		}
+  	}
   
-  public void setEtatPiece(boolean etat)
-  {
-  	this.etatPiece = etat;
-    if(etat == false)
-    {
-      this.x = 8;
-      this.y = 9;
-    }
-  }
-  
-  // Deplacer piece a redefinir dans les objects tour, cavalier etc...
-  public void deplacerPiece() {}
+	/*___________________FIN DES SETTERS	*/
+	
+  	// Deplacer piece a redefinir dans les objects tour, cavalier etc...
+  	public void deplacerPiece()
+	{
+	}
                        
-  /* Methode toString */              
-  public String toString()
-  {
-    return "X";
-  }
+ 	/* Methode toString */              
+  	public String toString()
+  	{
+    		return "X";
+  	}
 }
-
