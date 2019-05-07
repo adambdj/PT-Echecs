6f3f3e42903afedc169ec2f57324d4c6ead73d3e
Adam Bouarroudj <48649017+adambdj@users.noreply.github.com>
2019-05-07 23:52:43 +0200

    Harmonisation
    Harmonisation du code pour plus de clarté.

--- Joueur.java
+++ Joueur.java
@@ -1,32 +1,44 @@
-
-public class Joueur {
+public class Joueur
+{
 	private String prénom;
 	private int couleur;
 	private ArrayList<Piece> tabPiece;
 
-	public Joueur(String p, int c){
+	public Joueur(String p, int c)
+	{
 		this.prénom=p;
 		this.couleur=c;
 	}
 
 
-//Les accesseurs
-	public String getPrénom() {
+	//Les accesseurs
+	public String getPrénom()
+	{
 		return prénom;
 	}
-	public void setPrénom(String prénom) {
+	
+	public void setPrénom(String prénom)
+	{
 		this.prénom = prénom;
 	}
-	public int getCouleur() {
+	
+	public int getCouleur()
+	{
 		return couleur;
 	}
-	public void setCouleur(int couleur) {
+	
+	public void setCouleur(int couleur)
+	{
 		this.couleur = couleur;
 	}
-	public ArrayList<Piece> getTabPiece() {
+	
+	public ArrayList<Piece> getTabPiece()
+	{
 		return tabPiece;
 	}
-	public void setTabPiece(ArrayList<Piece> tabPiece) {
+	
+	public void setTabPiece(ArrayList<Piece> tabPiece)
+	{
 		this.tabPiece = tabPiece;
 	}
 
