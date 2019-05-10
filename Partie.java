ec4fa26912606b30a5d6818ab33c59ad01b2969a
souemya <40347869+souemya@users.noreply.github.com>
2019-05-10 02:40:54 +0200

    Ajout de méthodes
    En faisant le déplacement pour le pion j'ai du ajouteur les methodes suivante :
    positionInitialePion(Pion p, Joueur numJoueur) | Afin d'uriliser la reglé d'avance de 2 cases
    public Joueur joueurEnCour(Partie p)| Afin de retourner le joueur entrain de joueur utile pour la fonction précédente

--- Partie.java
+++ Partie.java
@@ -65,14 +65,40 @@ public class Partie
 		}
 		else // p est un pion 
 		{
-			//Si la case de devant+1 est libre ou la case devant+2 est libre 
-			if(this.terrain.etatCase(this.terrain.getCase(p.getPosX(), p.getPosY()+1)) || 
-					this.terrain.etatCase(this.terrain.getCase(p.getPosX(), p.getPosY()+2)) ) 
+			//Si la case de devant+1 est libre ou la case devant+2 quand on est en position initiale  
+			//Je dois créer une méthode positionIntiale
+			if ((this.positionInitialePion((Pion)p,this.joueurEnCour(this)) && this.terrain.etatCase(this.terrain.getCase(p.getPosX(), p.getPosY()+2))))	
+				reponse = true;
+			if(this.terrain.etatCase(this.terrain.getCase(p.getPosX(), p.getPosY()+1)) ) 
 				reponse = true;
 		}
-			
 		return reponse;
 		
+	}
+	
+	
+	/*Soumeya___ permet de vérifier i le pion placé en paramètre est positionné dans sa case initiale ou non 
+	 * On met en parametre le joueur car la position initiale est différente selon le joueur
+	 */
+	public boolean positionInitialePion(Pion p, Joueur numJoueur) {
+		for (int j=1;j<9;j++)
+			if(numJoueur == this.joueur1)
+				if(p.getPosX()==2 && p.getPosY() == j)
+					return true;	
+			else
+				if(p.getPosX()==7 && p.getPosY() == j)
+					return true;
+		
+		return false;
+	}
+	
+	/*	A FAIRE
+	 * Il faut une fonction qui renvoie le joueur qui est entrain de jouer 
+	 * Pour pouvoir utiliser la fonction positionInitialePion()
+	 */
+	
+	public Joueur joueurEnCour(Partie p) {
+		return joueur1;
 	}
 	
 //-------------------
