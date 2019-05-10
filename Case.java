bec1810001deb8f096018a6ce535249fff1e758e
salimdrch <salim.drch@gmail.com>
2019-05-10 17:51:24 +0200

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,51 +1,55 @@
 public class Case
 {
-  private int x; // position x
+    private int x; // position x
     private int y; // position y
     private Piece p; // case peut etre compose de piece
-      
+
   public Case(int x, int y)
   {
     this.x = x;
     this.y = y;
   }
-    
+
   public Case(int x, int y, Piece p)
       {
           this.x = x;
           this.y = y;
         this.p = p;  // sinon par exemple je fais des if(p instanceof cavalier)
       }
-  
-  /*Soumeya___Permet de savoir si la Case contient une piece ou non 
-   * Utile pour la fonction etatCase() dans la classe echiqier 
+  public Case(Piece p)
+  {
+    this.p = p;
+  }
+
+  /*Soumeya___Permet de savoir si la Case contient une piece ou non
+   * Utile pour la fonction etatCase() dans la classe echiqier
    */
-  
+
   public boolean estVide() {
     if(this.p!=null)
       return false;
     return true;
   }
-  
-  
+
+
     /* _________________________ACCESSEUR*/
-  
+
     public Piece getPiece()
       {
     return this.p;
       }
-    
+
     public int getPosX()
     {
       return this.x;
   }
-  
+
   public int getPosY()
   {
       return this.y;
-    } 
-  
-  
+    }
+
+
     public void setPosX(int x)
     {
           this.x = x;
@@ -55,22 +59,21 @@ public class Case
     {
         this.y = y;
     }
-  
+
     public void setPiece(Piece piece)
     {
       this.p = piece; // met la piece dans la case
     }
     /* _________________________________________________FIN ACCESSEUR*/
-  
+
   public String toString()
-  { 
-    String s="";
-    return s;
+  {
+    return "Case["+this.x + ","+this.y+ "]"+ this.p;
   }
-  
-  
-    /*  ---------------------------------------- VERIFIE SI LA SAISIE DE LA CASE EST VALIDE ------------------- 
-  
+
+
+    /*  ---------------------------------------- VERIFIE SI LA SAISIE DE LA CASE EST VALIDE -------------------
+
     public boolean saisieValide()
     {
       char[] tabChar = {"A","B","C","D","E","F","G","H"};
@@ -86,7 +89,7 @@ public class Case
         }
       }
     return p;
-    }   
+    }
       --------------------------------------------------------------------------------------------------------- */
-  
+
 }
