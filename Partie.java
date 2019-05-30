c5d1da1c097932f79cbf2d2dc3271f57d13cc6e6
Adam Bouarroudj <48649017+adambdj@users.noreply.github.com>
2019-05-31 01:41:09 +0200

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -14,16 +14,17 @@ public class Partie
 	
 //--------------Soumeya
 	/*Le jeu annonce un MAT lorsque le Roi est attaqué et qu’il n’y a aucune possibilité de le sauver.*/
-	public boolean verifMAT()
-	{
-	}
+
+	// public boolean verifMAT()
+	// {
+	// }
 
 	/*	Le jeu annonce un PAT lorsque le joueur ne peut déplacer aucune pièces 
 	 *	et que le roi n’est pas en attaque et ne peut se déplacer sans se mettre en échec .*/
 
-	public boolean verifPAT()
-	{
-	}
+	// public boolean verifPAT()
+	// {
+	// }
 	
 	/*public boolean verifDeplacement(Piece p)
 	{
