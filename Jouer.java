eff299f1e9842fe23c5950f6059fc240381a378e
souemya <40347869+souemya@users.noreply.github.com>
2019-06-02 14:35:00 +0200

    Test
    Comment sa donne si on utilise l'echiquier de la partie

--- Jouer.java
+++ Jouer.java
@@ -2,8 +2,20 @@ public class Jouer
 {
     public static void main(String[] args)
     {
-    	Echiquier e = new Echiquier();
-    	System.out.println(e.afficher());
+    	Scanner saisie = new Scanner(System.in);
+    	
+    	System.out.println("Joueur 1 : Votre nom ?");
+    	String j1nom = saisie.nextLine();
+    	Joueur j1 = new Joueur(j1nom,1);
+    	
+    	System.out.println("Joueur 2 : Votre nom ?");
+    	String j2nom = saisie.nextLine();
+    	Joueur j2 = new Joueur(j2nom,0);
+    	
+    	System.out.println(j1 + "\n" + j2);
+    	Partie pp = new Partie(j1,j2);
+
+    	System.out.println(pp.getTerrain().afficher());
     }
 
     /*
