14e389e046104cfd5a77955e05fb0c5fd134ef60
salimdrch <salim.drch@gmail.com>
2019-06-16 23:47:37 +0200

    u

--- TestRoi.java
+++ TestRoi.java
@@ -5,10 +5,9 @@ public class TestRoi
     Case d = e.getCase(5,3);
     Case a = e.getCase(4,3);
     e.setCase(d, new Roi(5,3,0));
-    e.setCase(a, new Pion(4,3,0));
-    System.out.println(e.afficher());
-    System.out.println();
-    d.getPiece().deplacerPieces(e,a);
+    e.setCase(a, new Pion(3,3,0));
+    System.out.println(d.getPiece().verifDeplacement(e,a));
+//    d.getPiece().deplacerPieces(e,a);
     System.out.println(e.afficher());
   }
 
