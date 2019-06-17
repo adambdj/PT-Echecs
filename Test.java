ce182e230542984554654c3fe7c528e65a3cd823
souemya <40347869+souemya@users.noreply.github.com>
2019-06-17 13:49:55 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,12 +5,26 @@ public class Test
   public static void main(String [] arg)
   {
     Echiquier e = new Echiquier();
-    e.setCase(e.getCase(4, 3), e.getCase(7, 4).getPiece());
-    e.getCase(7, 4).setPiece(null);
-    System.out.println(e.afficher());
+    Piece roi = new Roi(4, 4,1);
+        Piece pion = new Pion(2, 4,0);
+        Piece fou = new Fou(3, 5,0);
 
-    System.out.println(e.enEchec(e.getCase(4, 3).getPiece()));
-    e.listeEnnemie.clear();
+        /*Demo Echec : Il n'y a pas echec */
+        e.setCase(e.getCase(4, 4), roi);
+        e.setCase(e.getCase(2, 4), pion);
+        e.setCase(e.getCase(5, 4), fou);
+        
+        System.out.println(e.afficher());
+        System.out.println(e.enEchec(roi));
+        
+        /*Demo Echec : Il y a echec */
+        e.setCase(e.getCase(4, 4), roi);
+        e.setCase(e.getCase(3, 4), pion);
+        e.setCase(e.getCase(3, 5), fou);
+        
+        System.out.println(e.afficher());
+        System.out.println(e.enEchec(roi));
+        System.out.println("Se fait manger par le fou");
         
   }
 
