775367c27760d033a7f3e873d8dc907d5833e492
souemya <40347869+souemya@users.noreply.github.com>
2019-06-17 10:18:52 +0200

    Test Echec

--- Test.java
+++ Test.java
@@ -5,22 +5,13 @@ public class Test
   public static void main(String [] arg)
   {
     Echiquier e = new Echiquier();
-    Case depart = e.getCase(6,0);
-    Case arrive = e.getCase(4,0);
-
+    e.setCase(e.getCase(4, 3), e.getCase(7, 4).getPiece());
+    e.getCase(7, 4).setPiece(null);
     System.out.println(e.afficher());
-    System.out.println(depart);
-    System.out.println(arrive);
-    System.out.println(depart.getPiece());
-    System.out.println(depart.getPiece().verifDeplacement(e, arrive));
-    depart.getPiece().deplacerPieces(e,arrive);
 
-    System.out.println(e.afficher());
-    System.out.println(depart);
-    System.out.println(arrive);
-
-
-
+    System.out.println(e.enEchec(e.getCase(4, 3).getPiece()));
+    e.listeEnnemie.clear();
+        
   }
 
 }
